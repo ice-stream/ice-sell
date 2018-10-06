fe44f8800f8388bdff1a4fe0d940e14ce231602b
ice-stream <ice_tream@163.com>
2018-10-06 14:38:09 +0800

    房屋销售平台第一次提交

---
+++ src/test/java/com/ice/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.ice.service.impl;
+
+import com.ice.dataobject.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/9/18.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryServiceImplTest {
+
+
+    @Autowired
+    private CategoryServiceImpl categoryService;
+
+
+
+
+
+    @Test
+    public void FindOne() throws Exception {
+        ProductCategory productCategory = categoryService.findOne(1);
+        Assert.assertEquals(new Integer(1), productCategory.getCategoryId());
+    }
+
+    @Test
+    public void FindAll() throws Exception {
+        List<ProductCategory> productCategoryList = categoryService.findAll();
+        Assert.assertNotEquals(0, productCategoryList.size());
+    }
+
+    @Test
+    public void FindByCategoryTypeIn() throws Exception {
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1,2,3,4));
+        Assert.assertNotEquals(0, productCategoryList.size());
+    }
+
+    @Test
+    public void Save() throws Exception {
+        ProductCategory productCategory = new ProductCategory("男生专享", 10);
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertNotNull(result);
+    }
+}
